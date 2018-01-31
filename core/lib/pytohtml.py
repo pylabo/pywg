734e71a8bd8c6d6a3268df209140ba3dd2f9c6f4
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-30 22:04:34 -0500

    Work In Progress

--- core/lib/pytohtml.py
+++ core/lib/pytohtml.py
@@ -1,1 +1,10 @@
 class pytohtml():
+  def __init__(self,theme):
+    self.theme = theme
+    self.tree = {}
+    self.source = ''
+  def new(self,*oid**kwargs):
+    self.tree[oid[0]][oid[1]][oid[2]][oid] = {}
+    workdict = self.tree[oid[0]][oid[1]][oid[2]][oid]
+    workdict['type'] = kwargs['type']
+    # WIP
